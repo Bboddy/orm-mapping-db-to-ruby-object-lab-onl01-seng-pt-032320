fcff7c468a3fbd9495902312c336651682d98e44
Bryce B <bryce.boddy@gmail.com>
2020-06-10 06:45:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,4 +82,16 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.all_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
