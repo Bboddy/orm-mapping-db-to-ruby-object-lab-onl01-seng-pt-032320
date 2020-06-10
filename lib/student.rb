6aa7d01c62fcc56b524c12bbdb0c903d9eb357fb
Bryce B <bryce.boddy@gmail.com>
2020-06-10 06:06:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,8 +33,12 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = #{name} LIMIT 1"
-  end
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = 
+      LIMIT 1
+    SQL
  
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
